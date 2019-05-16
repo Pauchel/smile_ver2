d2ddc9bbd83ed16142192cf8551c08a21b0645e9
Ninom <nionanoni2038@gmail.com>
2019-05-16 19:14:47 +0900

    Initial Commit

---
+++ smile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smile
+//
+//  Created by 二宮啓 on 2019/05/16.
+//  Copyright © 2019 二宮啓. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
